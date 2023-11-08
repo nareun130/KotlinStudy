0aedaf7f9ef51fc03885b0917c909cd604fed518
김성호 <nareun130@gmail.com>
2023-11-09 03:58:20 +0900

    [FEAT] : 코틀린의 기본 가시성은 public 이다

--- 02-class/src/person/personKotlin.kt
+++ 02-class/src/person/personKotlin.kt
@@ -1,6 +1,6 @@
 package person
 
-class PersonKotlin(
+class PersonKotlin( //~> kotlin의 기본 가시성은 public!!
 	//* 기본이 val(읽기 전용 형태, java에서의 final)로 지정하고 쓴다. -> 함수형 프로그래밍과 연
 	val name: String,
 	val int: Integer,
