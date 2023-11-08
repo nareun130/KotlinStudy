d8b081743031f47e627c1dac4fe8f1824a664033
김성호 <nareun130@gmail.com>
2023-11-09 04:00:21 +0900

    [FEAT] : Kotlin에서는 Integer대신 Int사용

--- 02-class/src/person/personKotlin.kt
+++ 02-class/src/person/personKotlin.kt
@@ -1,9 +1,9 @@
 package person
 
-class PersonKotlin( //~> kotlin의 기본 가시성은 public!!
+class PersonKotlin( //~> kotlin의 기본 가시성은 public
 	//* 기본이 val(읽기 전용 형태, java에서의 final)로 지정하고 쓴다. -> 함수형 프로그래밍과 연
 	val name: String,
-	val int: Integer,
+	val int: Int,
 	//* var : 읽고 쓰기 가능 
 	var isMarried: Boolean
 
