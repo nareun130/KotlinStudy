a79885e648a04fd78f8082e155673c59516cd19b
김성호 <nareun130@gmail.com>
2023-11-10 01:25:46 +0900

    [FEAT] : 스마트 캐스트 함수 리팩토링

--- 02-class/src/cast/smart.kt
+++ 02-class/src/cast/smart.kt
@@ -6,18 +6,32 @@ interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
-fun eval(e: Expr): Int {
-	if (e is Num) {
-		val n = e as Num //? Num타입으로 변환 -> 불필요한 중복 
+//fun eval(e: Expr): Int {
+//	if (e is Num) {
+//		val n = e as Num //? Num타입으로 변환 -> 불필요한 중복 
+//
+//		return n.value
+//	}
+//	if (e is Sum) {
+//		return eval(e.right) + eval(e.left) //~> 변수 e에 대해 스마트 캐스트를 사용 
+//	}
+//	throw IllegalArgumentException("Unkown expression")
+//}
 
-		return n.value
+//리팩토링 : eval-if
+//fun eval(e: Expr): Int =
+//	if (e is Num) e.value
+//	else if (e is Sum) eval(e.left) + eval(e.right)
+//	else throw IllegalArgumentException("Unknown expression")
+
+//리팩토링 : eval-when
+fun eval(e: Expr): Int =
+	when (e) {
+		is Num -> e.value
+		is Sum -> eval(e.left) + eval(e.right)
+		else -> throw IllegalArgumentException("Unknown expression")
 	}
-	if (e is Sum) {
-		return eval(e.right) + eval(e.left) //~> 변수 e에 대해 스마트 캐스트를 사용 
-	}
-	throw IllegalArgumentException("Unkown expression")
-}
 
 fun main() {
-	println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+	println(eval(Sum(Sum(Num(1), Num(2)), Num(3))))
 }
